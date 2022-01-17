c7f970a590bbe9b0aacda0dd3e2fc9451440daab
Sergiy Wind <sergiy_viter@ukr.net>
2022-01-17 19:20:11 +0300

    secont commit

--- Console_app1/Console_app1.cpp
+++ Console_app1/Console_app1.cpp
@@ -1,11 +1,18 @@
 ﻿#include <iostream>
 
+void printSomeText();
+
 int main()
 {
-    
+    printSomeText();
 
 
 
 
     return 0;
+}
+
+void printSomeText()
+{
+    std::cout << "Some text in function\n";
 }
